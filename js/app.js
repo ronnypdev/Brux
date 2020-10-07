9228fd459b2f7f429d0ef2562aba01619809efa1
Ronny Perez <ronnypdev@gmail.com>
2020-10-06 20:43:19 -0400

    Adding a box shadow on scroll

--- js/app.js
+++ js/app.js
@@ -13,6 +13,21 @@ const toggleHeader = () => {
   }
 };
 
+const fadeBox = () => {
+  const pixels = window.pageYOffset;
+  const alpha = Math.min(pixels / 1000, 0.5);
+
+  headerTag.style.boxShadow = `0 0 10px rgba(0, 0, 0, ${alpha})`;
+};
+
+fadeBox();
 toggleHeader();
 
-document.addEventListener('scroll', toggleHeader, false);
+document.addEventListener(
+  'scroll',
+  function () {
+    toggleHeader();
+    fadeBox();
+  },
+  false
+);
