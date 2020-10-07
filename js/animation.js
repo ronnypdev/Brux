3c76329128734b635c4da9ec14e02064d8a1be4f
Ronny Perez <ronnypdev@gmail.com>
2020-10-07 11:08:26 -0400

    Adding animation on upwards scroll

--- js/animation.js
+++ js/animation.js
@@ -10,9 +10,19 @@ animatedTags.forEach((tag) => {
 const fadeIn = () => {
   // look through all the animated tags and see
   // with getBoundingClientRect if it's in the window
+  let delay = 0.25;
+
   animatedTags.forEach((tag) => {
     const tagTop = tag.getBoundingClientRect().top;
     const tagBottom = tag.getBoundingClientRect().bottom;
+
+    if (tagTop < window.innerHeight && tagBottom > 0) {
+      tag.style.animation = `fadein 1s ${delay}s both`;
+      delay = delay + 0.25;
+    } else {
+      tag.style.opacity = 0;
+      tag.style.animation = '';
+    }
   });
 };
 
